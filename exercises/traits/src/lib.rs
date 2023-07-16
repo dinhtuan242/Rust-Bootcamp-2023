c08d809deb3d1b5f9c09b6686eb7ba300ef0a6d8
dinhtuan242 <vudinhtuan242@gmail.com>
2023-07-16 11:36:52 +0700

    fix trait

--- exercises/traits/src/lib.rs
+++ exercises/traits/src/lib.rs
@@ -1,3 +1,4 @@
+use std::ops::Add;
 // Exercise 1
 // Fill in the two impl blocks to make the code work.
 // Make it compile
@@ -10,35 +11,48 @@ trait Hello {
     fn say_something(&self) -> String;
 }
 
-//TODO 
+//TODO
 struct Student {}
 impl Hello for Student {
+    fn say_hi(&self) -> String {
+        "hi".to_string()
+    }
+    fn say_something(&self) -> String {
+        "I'm a good student".to_string()
+    }
 }
 //TODO
 struct Teacher {}
 impl Hello for Teacher {
+    fn say_hi(&self) -> String {
+        "Hi, I'm your new teacher".to_string()
+    }
+    fn say_something(&self) -> String {
+        "I'm not a bad teacher".to_string()
+    }
 }
-
 
 // Exercise 2
 // Make it compile in unit test for exercise 2
-// Hint: use #[derive]  for struct Point 
+// Hint: use #[derive]  for struct Point
 // Run tests
+#[derive(PartialEq, Debug)]
 struct Point {
     x: i32,
     y: i32,
 }
 
-
 // Exercise 3
-// Make it compile 
+// Make it compile
 // Implement `fn sum` with trait bound in two ways.
 // Run tests
 // Hint: Trait Bound
-fn sum<T>(x: T, y: T) -> T {
+fn sum<T>(x: T, y: T) -> T
+where
+    T: Add<Output = T> + Copy,
+{
     x + y
 }
-
 
 // Exercise 4
 // Fix errors and implement
@@ -49,25 +63,29 @@ trait Foo {
 }
 
 impl Foo for u8 {
-    fn method(&self) -> String { format!("u8: {}", *self) }
+    fn method(&self) -> String {
+        format!("u8: {}", *self)
+    }
 }
 
 impl Foo for String {
-    fn method(&self) -> String { format!("string: {}", *self) }
+    fn method(&self) -> String {
+        format!("string: {}", *self)
+    }
 }
 
 // IMPLEMENT below with generics and parameters
-fn static_dispatch(x) {
-    todo!()
+fn static_dispatch<T: std::fmt::Debug>(x: T) {
+    println!("Static Dispatch: {:?}", x);
 }
 
 // Implement below with trait objects and parameters
-fn dynamic_dispatch(x) {
-    todo!()
+fn dynamic_dispatch<T: std::fmt::Display>(x: &T) {
+    println!("Dynamic Dispatch: {}", x);
 }
 
-// Exercise 5 
-// Fix errors and fill in the blanks 
+// Exercise 5
+// Fix errors and fill in the blanks
 // Run tests
 // Hint: &dyn and Box<dyn>
 trait Draw {
@@ -87,15 +105,15 @@ impl Draw for f64 {
 }
 
 fn draw_with_box(x: Box<dyn Draw>) {
-    x.draw();
+    println!("{}", x.draw());
 }
 
-fn draw_with_ref(x: __) {
-    x.draw();
+fn draw_with_ref<T: Draw>(x: T) {
+    println!("{}", x.draw());
 }
 
 // Exercise 6
-// Fix errors and implement 
+// Fix errors and implement
 // Run tests
 // Hint: Associated Type
 
@@ -106,13 +124,24 @@ trait Container {
     fn is_empty(&self) -> bool;
 }
 
-struct Stack {
-    items: Vec<u8>,
+struct Stack<T> {
+    items: Vec<T>,
 }
 
 //TODO implement Container for Stack
+impl<T> Stack<T> {
+    fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
 
+    fn insert(&mut self, item: T) {
+        self.items.push(item);
+    }
 
+    fn remove(&mut self) -> Option<T> {
+        self.items.pop()
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -125,10 +154,10 @@ mod tests {
         let s = Student {};
         assert_eq!(s.say_hi(), "hi");
         assert_eq!(s.say_something(), "I'm a good student");
-    
+
         let t = Teacher {};
         assert_eq!(t.say_hi(), "Hi, I'm your new teacher");
-        assert_eq!(t.say_something(), "I'm not a bad teacher");   
+        assert_eq!(t.say_something(), "I'm not a bad teacher");
     }
 
     #[test]
@@ -150,25 +179,25 @@ mod tests {
     fn exercise4_should_work() {
         let x = 5u8;
         let y = "Hello".to_string();
-    
+
         static_dispatch(x);
-        dynamic_dispatch(&y); 
+        dynamic_dispatch(&y);
     }
 
     #[test]
     fn exercise5_should_work() {
         let x = 1.1f64;
         let y = 8u8;
-    
+
         // Draw x.
-        draw_with_box(__);
-    
+        draw_with_box(Box::new(x));
+
         // Draw y.
-        draw_with_ref(&y);
+        draw_with_ref(y);
     }
 
     #[test]
-    fn exercise6_should_work(){
+    fn exercise6_should_work() {
         let mut stack: Stack<u8> = Stack { items: Vec::new() };
         assert!(stack.is_empty());
         stack.insert(1);
@@ -181,5 +210,4 @@ mod tests {
         assert_eq!(stack.remove(), None);
         assert!(stack.is_empty());
     }
-
 }
